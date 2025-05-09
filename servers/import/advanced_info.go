41af3ced0a146399a65d88de5b60695cf41a2706
Josh Pattman <Josh.pattman@gmail.com>
2025-05-09 21:50:18 +0100

    Add actual advanced import

--- servers/import/advanced_info.go
+++ servers/import/advanced_info.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"os"
 	"utils"
 
+	_ "embed"
+
+	"github.com/JoshPattman/jpf"
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,9 +27,54 @@ func advancedInfoHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, imported)
 }
 
-func advancedInfoFromRecipe(utils.Recipe) (utils.RecipeImportInfo, error) {
+func advancedInfoFromRecipe(recipie utils.Recipe) (utils.RecipeImportInfo, error) {
+	model := jpf.NewStandardOpenAIModel(os.Getenv("OPENAI_KEY"), "gpt-4o-mini", 0, 0, 0.4)
+	summariser := NewRecipieSummariser()
+	summary, _, err := jpf.RunOneShot(model, summariser, recipie)
+	if err != nil {
+		return utils.RecipeImportInfo{}, err
+	}
+	embedder := jpf.NewOpenAIEmbedder(os.Getenv("OPENAI_KEY"), "text-embedding-3-small")
+	vec, err := embedder.Embed(summary)
+	if err != nil {
+		return utils.RecipeImportInfo{}, err
+	}
 	return utils.RecipeImportInfo{
-		Summary: "A recipie",
-		Vector:  []float64{0},
+		Summary: summary,
+		Vector:  vec,
 	}, nil
+}
+
+var _ jpf.Function[utils.Recipe, string] = &recipieSummariser{}
+
+func NewRecipieSummariser() jpf.Function[utils.Recipe, string] {
+	return &recipieSummariser{}
+}
+
+//go:embed prompts/recipie_summarise.md
+var recipieSummariserPrompt string
+
+type recipieSummariser struct{}
+
+// BuildInputMessages implements jpf.Function.
+func (r *recipieSummariser) BuildInputMessages(recipie utils.Recipe) ([]jpf.Message, error) {
+	content, err := json.MarshalIndent(recipie, "", "  ")
+	if err != nil {
+		return nil, err
+	}
+	return []jpf.Message{
+		{
+			Role:    jpf.SystemRole,
+			Content: recipieSummariserPrompt,
+		},
+		{
+			Role:    jpf.UserRole,
+			Content: string(content),
+		},
+	}, nil
+}
+
+// ParseResponseText implements jpf.Function.
+func (r *recipieSummariser) ParseResponseText(s string) (string, error) {
+	return s, nil
 }
--- servers/import/go.mod
+++ servers/import/go.mod
@@ -5,6 +5,7 @@ go 1.24.3
 require github.com/gin-gonic/gin v1.10.0
 
 require (
+	github.com/JoshPattman/jpf v0.0.0-20250508203619-1800192d5857 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
