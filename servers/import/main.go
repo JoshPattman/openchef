def43df865f79141f76a8d81416226566b6d5864
Josh Pattman <Josh.pattman@gmail.com>
2025-05-09 19:59:19 +0100

    Add server to import

--- servers/import/main.go
+++ servers/import/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world")
+	port := flag.Int("port", 8080, "Port to run on")
+	flag.Parse()
+
+	gin.SetMode(gin.ReleaseMode)
+
+	r := gin.Default()
+	r.GET("/healthcheck", healthCheck)
+
+	err := r.Run(fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func healthCheck(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, "OK")
 }
