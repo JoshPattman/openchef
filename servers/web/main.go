138119fd7044bdf5aab33765b333b2ac82e6c57d
Jack Darlison <jdarlison7@gmail.com>
2025-05-09 20:35:24 +0100

    base web

--- servers/web/main.go
+++ servers/web/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
 	db, err := ConnectToDB()
 	if err != nil {
@@ -9,4 +17,27 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	port := flag.Int("port", 8080, "Port to run on")
+	flag.Parse()
+
+	gin.SetMode(gin.ReleaseMode)
+
+	r := gin.Default()
+	r.GET("/healthcheck", healthCheckHandler)
+	r.GET("/get/*website", getWebsite)
+
+	err = r.Run(fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func healthCheckHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, "OK")
+}
+
+func getWebsite(ctx *gin.Context) {
+	website := ctx.Param("website")
+	ctx.JSON(http.StatusOK, gin.H{"website": website})
 }
